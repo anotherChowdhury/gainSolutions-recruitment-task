4dd8bc58719a5d7157ac2111d1ed87de5283cf86
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-06-19 14:46:29 +0600

    added student model

---
+++ src/models/student.js
@@ -0,0 +1,11 @@
+import { model, Schema } from 'mongoose'
+const { Types } = Schema
+
+const studentSchema = new Schema({
+  _id: Types.ObjectId,
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  subjects: { type: Types.ObjectId, ref: 'Subject' },
+})
+
+export default model('Student', studentSchema)
