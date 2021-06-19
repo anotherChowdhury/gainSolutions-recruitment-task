ca30f3a58dcbe1f1a0a1f9c7eea28bc45663c57d
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-06-19 18:13:31 +0600

    added student resolver

---
+++ src/graphql/resolvers/student.js
@@ -0,0 +1,30 @@
+import student from '../../models/student'
+import { StudentController, SubjectController } from '../../server'
+
+export default {
+  getStudents: () => StudentController.get({}),
+  getStudent: (_, { studentId }) => StudentController.getById(studentId),
+  createStudent: (_, { name, email, birthDate, subjects }) =>
+    StudentController.add({ name, email, birthDate, subjects }),
+  addSubjectToStudent: async (_, { studentId, subjects }) => {
+    const added = await StudentController.addSubjects(studentId, subjects)
+    for (const subId of subjects) {
+      SubjectController.addStudents(subId, [studentId])
+    }
+    return true
+  },
+  removeSubjectFromStudent: async (_, { studentId, subjectId }) => {
+    await StudentController.removeSubjectFromStudent(studentId, subjectId)
+    await SubjectController.removeStudentFromSubject(subjectId, studentId)
+    return true
+  },
+  deleteStudent: async (_, { studentId }) => {
+    const stundent = await StudentController.getById(studentId)
+    if (!student) return 'No Students Found'
+    if (student.subjects.length > 0)
+      for (const subId of student.subjects) {
+        SubjectController.removeStudentFromSubject(subId, studentId)
+      }
+    return StudentController.deleteById(studentId)
+  },
+}
