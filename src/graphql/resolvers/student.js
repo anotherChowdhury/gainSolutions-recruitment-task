0aacd5a882cc892711f6afc6483aafa06305ea25
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-06-19 18:42:18 +0600

    added getAllSubjectsOfAStudent

--- src/graphql/resolvers/student.js
+++ src/graphql/resolvers/student.js
@@ -1,11 +1,16 @@
-import student from '../../models/student'
 import { StudentController, SubjectController } from '../../server'
 
 export default {
   getStudents: () => StudentController.get({}),
+
   getStudent: (_, { studentId }) => StudentController.getById(studentId),
+
+  getAllSubjectByAStudent: (student, {}) =>
+    StudentController.getAllSubjectByAStudent(student.id),
+
   createStudent: (_, { name, email, birthDate, subjects }) =>
     StudentController.add({ name, email, birthDate, subjects }),
+
   addSubjectToStudent: async (_, { studentId, subjects }) => {
     const added = await StudentController.addSubjects(studentId, subjects)
     for (const subId of subjects) {
@@ -13,13 +18,15 @@ export default {
     }
     return true
   },
+
   removeSubjectFromStudent: async (_, { studentId, subjectId }) => {
     await StudentController.removeSubjectFromStudent(studentId, subjectId)
     await SubjectController.removeStudentFromSubject(subjectId, studentId)
     return true
   },
+
   deleteStudent: async (_, { studentId }) => {
-    const stundent = await StudentController.getById(studentId)
+    const student = await StudentController.getById(studentId)
     if (!student) return 'No Students Found'
     if (student.subjects.length > 0)
       for (const subId of student.subjects) {
