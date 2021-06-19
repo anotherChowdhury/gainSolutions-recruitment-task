ab2fe1ec453038e7be801d7de67ffb0fd656aadb
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-06-19 15:29:04 +0600

    added baseController

---
+++ src/controllers/baseController.js
@@ -0,0 +1,31 @@
+import { Types } from 'mongoose'
+class BaseController {
+  constructor(model) {
+    this._model = model
+  }
+
+  async add(properties) {
+    return this._model.create({
+      _id: Types.ObjectId(),
+      ...properties,
+    })
+  }
+
+  async getById(id) {
+    return this._model.findById(id)
+  }
+
+  async deleteById(id) {
+    return this._model.deleteById(id)
+  }
+
+  async getByName(name) {
+    return this._model.findOne({ name })
+  }
+
+  async get(conditions) {
+    return this._model.find(conditions)
+  }
+}
+
+export default BaseController
