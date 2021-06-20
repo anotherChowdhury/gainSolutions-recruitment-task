a794639b8a7e10d7c8773219e201dc8e62486a9b
anotherChowdhury <umarfaruk.chowdhury@gmail.com>
2021-06-20 17:23:42 +0600

    added cors

--- src/server.js
+++ src/server.js
@@ -4,15 +4,37 @@ import Subject from './models/subject'
 import StudentController from './controllers/student'
 import SubjectController from './controllers/subject'
 import connectToDatabase from './connectToDatabase'
+import { ApolloServer } from 'apollo-server-express'
+import StudentTypeDefs from './graphql/types/student'
+import SubjectTypeDefs from './graphql/types/subject'
+import IndexResolver from './graphql/resolvers/index.resolver'
 
 const studentController = new StudentController(Student)
 const subjectController = new SubjectController(Subject)
 
 connectToDatabase()
 const app = express()
+app.use(cors())
 
-export default {
-  studentController,
-  subjectController,
-  app,
-}
+const Query = `
+  type Query {
+    _empty: String
+  }
+`
+
+const Mutation = `
+  type Mutation {
+    _empty: String
+  }
+`
+
+const apolloServer = new ApolloServer({
+  typeDefs: [Query, Mutation, StudentTypeDefs, SubjectTypeDefs],
+  resolvers: IndexResolver,
+})
+
+apolloServer.applyMiddleware({ app })
+
+app.listen(5000, () => console.log('Listning on port 5000'))
+
+export { app, studentController, subjectController }
